a3c66e78893bdfdf39a051dcb54c901693343eb7
Mikhail Bautin <mbautin@users.noreply.github.com>
2024-01-11 00:17:55 -0800

    Improve reporting

--- monitor_metrics.py
+++ monitor_metrics.py
@@ -38,6 +38,11 @@ def main():
     metrics_script_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         'show_metrics.sh')
+
+    first_ts = None
+    prev_counter = None
+    prev_ts = None
+
     while True:
         # Check if the process is still running
         if not os.path.exists("/proc/" + str(args.process_id)):
@@ -57,6 +62,9 @@ def main():
                 timestamp_from_log_str = timestamp_from_log_str[:-4]
             parsed_ts_from_log = datetime.strptime(timestamp_from_log_str, TIME_FORMAT)
             current_ts = datetime.now()
+            if first_ts is None:
+                first_ts = parsed_ts_from_log
+
             delta_sec = abs((current_ts - parsed_ts_from_log).total_seconds())
 
             counter_value = int(match.group(2))
@@ -64,13 +72,25 @@ def main():
             actual_time_str = current_ts.strftime(TIME_FORMAT)
             logging.info(f"Current timestamp: {actual_time_str}")
             logging.info(f"Timestamp from bulk load log: {timestamp_from_log_str}")
+            elapsed_time_sec = (parsed_ts_from_log - first_ts).total_seconds()
+            logging.info("Elapsed time since first batch: %.3f sec", elapsed_time_sec)
+            if prev_ts is not None:
+                time_since_last_batch = (parsed_ts_from_log - prev_ts).total_seconds()
+                logging.info("Time since previous batch: %.3f sec", time_since_last_batch)
+                if time_since_last_batch > 0:
+                    logging.info(
+                        "Rows inserted per second: %.3f",
+                        (counter_value - prev_counter) / time_since_last_batch)
+
+            prev_ts = parsed_ts_from_log
+            prev_counter = counter_value
 
             if delta_sec >= 2:
                 logging.warning(
-                    "LARGE DELTA BETWEEN CURRENT TIMESTAMP AND TIMESTAMP FROM LOG: %.3f",
+                    "LARGE DELTA BETWEEN CURRENT TIMESTAMP AND TIMESTAMP FROM LOG: %.3f sec",
                     delta_sec)
             else:
-                logging.info("Acceptable delta between timestamps: %.3f", delta_sec)
+                logging.info("Acceptable delta between timestamps: %.3f sec", delta_sec)
 
             # Execute show_metrics.sh and pipe output to the log
             try:
