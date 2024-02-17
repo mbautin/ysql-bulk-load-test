ac559c146811b19a1742d38da777db121a4c9e10
Mikhail Bautin <mbautin@users.noreply.github.com>
2024-02-17 05:14:30 +0000

    Rework metrics collection

--- show_metrics.sh
+++ show_metrics.sh
@@ -6,15 +6,24 @@ set -x
 iostat
 
 set +x
-METRICS_RE="\
-rocksdb_bloom_filter_useful|\
-rocksdb_bloom_filter_checked|\
-rocksdb_block_cache_data_hit|\
-rocksdb_block_cache_data_miss\
-"
+METRICS_LIST=(
+rocksdb_bloom_filter_useful
+rocksdb_bloom_filter_checked
+rocksdb_block_cache_data_hit
+rocksdb_block_cache_data_miss
+)
+
+metrics_re=""
+for metric in "${METRICS_LIST[@]}"; do
+  if [[ -n ${metrics_re} ]]; then
+    metrics_re+="|"
+  fi
+  metrics_re+=$metric
+done
+
 curl -s http://localhost:9000/prometheus-metrics |
   grep -E "test_table" |
-  grep -wE "${METRICS_RE}" |
+  grep -wE "${metrics_re}" |
   sed 's/expo.*9000..//' |
   sed 's/[0-9]*$//' |
   tr " " "\n"
